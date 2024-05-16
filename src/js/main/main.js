83704dabf1f234a923c71c4b4be549ef4f4b083d
devingnow <https://github.com/devingnow>
2024-05-16 19:47:29 +0900

    0516 오후 footer끝

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -49,13 +49,16 @@ class GameListScroll {
     init() {
         this.game_list_scroll = document.createElement("div")
         this.prev = document.createElement("button")
+        this.prev.className = "prevBtn";
         this.scroll_bar = document.createElement("div")
         this.scrollBtn = document.createElement("span")
         this.next = document.createElement("button")
+        this.next.className = "nextBtn";
         this.game_list_scroll.className = "game_list_scroll";
         this.scroll_bar.className = "scroll_bar";
-        this.prev.innerHTML = "<";
-        this.next.innerHTML = ">";
+        this.prev.innerHTML = "";
+        this.next.innerHTML = "";
+
         this.scroll_bar.append(this.scrollBtn);
         this.game_list_scroll.append(this.prev,this.scroll_bar,this.next);
         this.root.append(this.game_list_scroll);
@@ -66,19 +69,18 @@ class GameListScroll {
         this.scrollBarWidth = this.scroll_bar.clientWidth;
     }
 
+
     event(){
         this.scrollBtn.onmousedown = (e) => {
             this.scrollTarget = e.target;
-            this.click = e.x - this.scrollBtn.getBoundingClientRect().left;
-            // console.log(click);
         }
         window.addEventListener("mousemove", (e) => {
             if(this.scrollTarget){
                 const _left = (e.x - this.startPosX) - this.click ;
                 if((_left <= 0) || (_left >= (this.scrollBarWidth - this.scrollBtn.clientWidth))) return
                 this.scrollTarget.style.left = `${_left}px`
-                const a = _left/this.scrollBarWidth *100;
-                this.categoryList.scrollLeft =(this.categoryList.scrollWidth- this.categoryList.offsetWidth) * a / 100;
+                const a = Math.round(_left/(this.scrollBarWidth - this.scrollBtn.clientWidth) * 100);
+                this.categoryList.scrollLeft =(this.categoryList.scrollWidth - this.categoryList.offsetWidth ) * a / 100;
             }
         })
         
@@ -87,5 +89,34 @@ class GameListScroll {
         })
     }
 }
-
 const gameListScroll = new GameListScroll(rec_category,categoryList)
+
+// window.addEventListener('DOMContentLoaded', function() {
+//     const scrollBtn = document.querySelector('nextBtn');
+//     console.log(scrollBtn);
+
+//     // 버튼 클릭 이벤트 핸들러
+//     scrollBtn.addEventListener('click', function() {
+//         // 현재 스크롤 위치
+//         var currentScroll = window.scrollX;
+
+//         // 뷰포트의 높이
+//         var viewportWidth = this.game_list_scroll.innerWidth;
+
+//         // 문서의 전체 높이
+//         var documentWidth = document.body.clientWidth;
+
+//         // 스크롤할 양을 계산 (문서 높이의 16%)
+//         var scrollAmount = documentWidth * 0.16;
+
+//         // 스크롤 위치를 현재 위치 + 16%로 이동
+//         window.scrollTo(currentScroll + scrollAmount,0);
+//         console.log(currentScroll)
+//     });
+// });
+const scrollBtn = document.querySelector('nextBtn');
+
+scrollBtn.onclick = (e) => {
+    const gamelistscroll = this.game_list_scroll.innerWidth;
+    const scrollAmount = this.scrollBarWidth * 0.16;
+}
