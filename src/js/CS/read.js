bb323d154d95bc67ac3ce1c4bd1be4a1b0d0bebc
MinhyeokChoco <https://github.com/MinhyeokChoco>
2024-05-21 11:34:50 +0900

    [comment] : 주석 내용 변경, 코드 내용 수정

--- src/js/CS/read.js
+++ src/js/CS/read.js
@@ -5,18 +5,8 @@ function render() { // render 함수 선언
     // 변수명을 임시로 사용 할 때 변수명 앞에 언더바를 붙임
     if (list !== null) {
         for (let i = 0; i < writeList.length; i++) { // writeList 배열 길이 만큼 반복문 실행
-            const _list = document.createElement("div");
-            // span 태그를 담을 div 요소 생성, 그리고 _list에 할당
+            const _list = document.createElement("div"); // span 태그를 담을 div 요소 생성, 그리고 _list에 할당
 
-            // const _num = document.createElement("span"); // 고객 문의 글의 번호를 담은 span 태그 요소 생성, 변수에 할당
-            // const _title = document.createElement("span"); // 제목 span
-            // const _content = document.createElement("span"); // 내용 span
-            // const _nickname = document.createElement("span"); // 작성자 span
-            // const _date = document.createElement("span"); // 작성일 span
-            // const _views = document.createElement("span"); // 조회수 span
-            // const _view = document.createElement("span"); // 상세 페이지 들어가는 버튼 생성
-            // _view.dataset.index = i + "" // _view의 데이터셋 값에 i를 문자열로 할당
-            // _list.append(_num, _title, _content, _nickname, _date, _views, _view); // _list 변수 안에 즉 div 요소 안에 생성시킨 span 하나하나 담는다.
             _list.innerHTML = `
             <span>${i + 1}</span>
             <span>${writeList[i].title}</span>
@@ -26,22 +16,37 @@ function render() { // render 함수 선언
             <span>${writeList[i].views}</span>
             <span data-index="${i}">⭐️</span>
             `
-            console.log(_list.childNodes);
-            _list.lastElementChild.onclick = (e) => { // 클릭 했을 때 발생되는 이벤트 작성
+            // 작성한 글의 번호, 제목, 내용, 작성자, 작성일, 조회수, 보기 버튼을 _list에 입력
+
+            _list.lastElementChild.onclick = (e) => { // _list의 마지막 자식요소를 클릭 했을 때 발생되는 이벤트 작성
                 sessionStorage.setItem("viewIndex", e.target.dataset.index) // 세션스토리지에 viewIndex 라는 키 안에 이벤트가 발생되는 데이터셋의 값을 저장한다.
                 writeList[i].views++; // writeList 배열의 i번째 요소의 views 속성을 1씩 증가 시킵니다. (조회수)
                 localStorage.setItem("csData", JSON.stringify(writeList)); // writeList 배열을 JSON 문자형으로 변환해서 로컬스토리지에 csData라는 키 안에 값으로 저장 합니다.
                 location.href = `view.html?index=${viewIndex}` // 상세 페이지로 이동
             }
-            // _num.innerHTML = i + 1; // 번호는 0부터 시작하므로 1 증가 후 할당
-            // _title.innerHTML = writeList[i].title; // writeList 배열 안에 i번째의 title 속성
-            // _content.innerHTML = writeList[i].content; // 내용
-            // _nickname.innerHTML = writeList[i].nickname; // 작성자
-            // _date.innerHTML = writeList[i].date; // 작성일
-            // _views.innerHTML = writeList[i].views; // 조회수
-            // _view.innerHTML = "⭐️" // 보기 버튼
-            list.append(_list) // span을 담은 div를 다른 div list에 담는다.
+
+            list.append(_list) // span을 담은 div를 list에 담는다.
         }
     }
 }
 render();
+
+// 원래 9번 줄
+// const _num = document.createElement("span"); // 고객 문의 글의 번호를 담은 span 태그 요소 생성, 변수에 할당
+// const _title = document.createElement("span"); // 제목 span
+// const _content = document.createElement("span"); // 내용 span
+// const _nickname = document.createElement("span"); // 작성자 span
+// const _date = document.createElement("span"); // 작성일 span
+// const _views = document.createElement("span"); // 조회수 span
+// const _view = document.createElement("span"); // 상세 페이지 들어가는 버튼 생성
+// _view.dataset.index = i + "" // _view의 데이터셋 값에 i를 문자열로 할당
+// _list.append(_num, _title, _content, _nickname, _date, _views, _view); // _list 변수 안에 즉 div 요소 안에 생성시킨 span 하나하나 담는다.
+
+// 원래 27번 줄
+// _num.innerHTML = i + 1; // 번호는 0부터 시작하므로 1 증가 후 할당
+// _title.innerHTML = writeList[i].title; // writeList 배열 안에 i번째의 title 속성
+// _content.innerHTML = writeList[i].content; // 내용
+// _nickname.innerHTML = writeList[i].nickname; // 작성자
+// _date.innerHTML = writeList[i].date; // 작성일
+// _views.innerHTML = writeList[i].views; // 조회수
+// _view.innerHTML = "⭐️" // 보기 버튼
